feca2706f75e870f6161d5135b66ae5424ffdf06
Matheus Silva <matheus.silva@thomsonreuters.com>
2017-09-01 22:34:38 -0300

    initial commit

---
+++ src/main/java/br/capiroto/names/capirotonames/CapirotoNamesApplication.java
@@ -0,0 +1,12 @@
+package br.capiroto.names.capirotonames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapirotoNamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapirotoNamesApplication.class, args);
+	}
+}
---
+++ src/test/java/br/capiroto/names/capirotonames/CapirotoNamesApplicationTests.java
@@ -0,0 +1,16 @@
+package br.capiroto.names.capirotonames;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CapirotoNamesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
